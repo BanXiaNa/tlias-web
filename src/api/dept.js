dd5367e87d3d9411aa9d076d571bfbbc00f3c6b6
_Ban_Xia_ <1943284256@qq.com>
2025-06-23 18:55:56 +0800

    feat(dept): 新增部门管理功能
    - 添加部门列表查询、新增、修改、删除接口
    - 实现部门列表展示和新增部门功能
    - 增加表单验证和消息提示
    -优化页面布局和样式

--- src/api/dept.js
+++ src/api/dept.js
@@ -3,3 +3,11 @@ import request from "@/utils/request";
 // 部门列表查询
 export const queryDeptList = () => request.get('/depts')
 
+// 新增部门
+export const addDept = (deptName) => request.post('/depts',deptName)
+
+// 修改部门
+export const updateDept = (dept) => request.put('/depts',dept)
+
+// 删除部门
+export const deleteDept = (deptId) => request.delete(`/depts/${deptId}`)
--- src/views/dept/index.vue
+++ src/views/dept/index.vue
@@ -1,15 +1,29 @@
 <script setup>
 import PageTitle from "@/views/style/pageTitle.vue";
 import { ref, onMounted } from "vue";
-import { Delete, Edit } from "@element-plus/icons-vue";
-import { queryDeptList } from "@/api/dept";
+import { CirclePlus, Delete, Edit } from "@element-plus/icons-vue";
+import { queryDeptList,
+  addDept,
+  updateDept,
+  deleteDept
+} from "@/api/dept";
+import { ElMessage } from "element-plus";
 
-// 钩子函数
+/**
+ * 钩子函数
+ * @author BanXia
+ */
 onMounted(() => {
   getDeptList();
 });
 
-// 查询所有部门
+/**
+ * 部门列表
+ * @author BanXia
+ */
+// 部门列表数据模型
+const DeptList = ref([]);
+// 获取部门列表
 const getDeptList = async () => {
   const res = await queryDeptList();
   if (res.code) {
@@ -17,8 +31,56 @@ const getDeptList = async () => {
   }
 };
 
-// 存储部门列表
-const DeptList = ref([]);
+/**
+ * Dialog表单
+ * @author BanXia
+ */
+// Dialog表单数据
+const dialogFormVisible = ref(false);
+// 表单标题
+const dialogTitle = ref("")
+// 表单数据模型
+const dept = ref({
+  name: ''
+});
+// 设置标题
+const setDialogTitle = (title) => {
+  dialogTitle.value = title
+}
+// 弹出表单
+const showDialog = () => {
+  dialogFormVisible.value = true
+};
+// 关闭表单
+const closeDialog = () => {
+  dialogFormVisible.value = false
+  dept.value = {}
+};
+// 新增部门
+const addEmp = () => {
+  showDialog()
+  setDialogTitle('新增部门')
+}
+// 保存
+const save = async () => {
+  const res = await addDept(dept.value)
+
+  if(res.code){// 成功
+    ElMessage.success("添加成功")
+    closeDialog()
+    await getDeptList()
+  }else {// 失败
+    ElMessage.error(res.msg)
+  }
+}
+// 校验
+const rules = {
+  name: [
+    { required: true, message: "请输入部门名称", trigger: "blur" },
+    { min: 2, max: 10, message: "长度在 2 到 10 个字符之间", trigger: "blur" }
+  ]
+};
+
 </script>
 
 <template>
@@ -26,8 +88,11 @@ const DeptList = ref([]);
   <PageTitle>部门管理</PageTitle>
 
 <!--  添加部门按钮-->
+<!--  触发Dialog对话框-->
   <div class="container">
-    <el-button type="primary">添加部门</el-button>
+    <el-button type="primary" @click = "addEmp()">
+      <el-icon><CirclePlus /></el-icon>新增部门
+    </el-button>
   </div>
 
 <!--  数据表格-->
@@ -60,6 +125,17 @@ const DeptList = ref([]);
   </div>
 
 <!--新增&修改对话框-->
+  <el-dialog v-model="dialogFormVisible" :title="dialogTitle" width="500">
+    <el-form :model="dept" :rules="rules">
+      <el-form-item label="部门名称" :label-width="80" prop="name">
+        <el-input v-model="dept.name"/>
+      </el-form-item>
+    </el-form>
+    <template #footer>
+        <el-button @click="closeDialog">取消</el-button>
+        <el-button type="primary" @click="save">确定</el-button>
+    </template>
+  </el-dialog>
 </template>
 
 <style scoped>
