d8f833359a87b2e5a1802be8a6bda83c2ebbcc9b
_Ban_Xia_ <1943284256@qq.com>
2025-06-23 21:48:10 +0800

    feat(dept): 新增部门更新功能
    - 添加查询部门详情接口和相关方法
    - 实现部门更新功能，包括表单回显和提交逻辑
    - 优化新增部门流程，增加表单校验
    - 调整页面布局，修复部分显示问题

--- src/api/dept.js
+++ src/api/dept.js
@@ -3,6 +3,9 @@ import request from "@/utils/request";
 // 部门列表查询
 export const queryDeptList = () => request.get('/depts')
 
+// 根据id查询部门
+export const queryDeptById = (deptId) => request.get(`/depts/${deptId}`)
+
 // 新增部门
 export const addDept = (deptName) => request.post('/depts',deptName)
 
--- src/views/dept/index.vue
+++ src/views/dept/index.vue
@@ -3,6 +3,7 @@ import PageTitle from "@/views/style/pageTitle.vue";
 import { ref, onMounted } from "vue";
 import { CirclePlus, Delete, Edit } from "@element-plus/icons-vue";
 import { queryDeptList,
+  queryDeptById,
   addDept,
   updateDept,
   deleteDept
@@ -35,14 +36,26 @@ const getDeptList = async () => {
  * Dialog表单
  * @author BanXia
  */
-// Dialog表单数据
+// Dialog表单是否显示
 const dialogFormVisible = ref(false);
 // 表单标题
 const dialogTitle = ref("")
+// 表单校验规则
+const rules = {
+  name: [
+    { required: true, message: "请输入部门名称", trigger: "blur" },
+    { min: 2, max: 10, message: "长度在 2 到 10 个字符之间", trigger: "blur" }
+  ]
+};
+// 表单响应对象
+const deptForm = ref();
 // 表单数据模型
-const dept = ref({
+const deptData = ref({
+  id : '',
   name: ''
 });
+// 确定按钮执行的操作
+const submit = ref();
 // 设置标题
 const setDialogTitle = (title) => {
   dialogTitle.value = title
@@ -53,33 +66,95 @@ const showDialog = () => {
 };
 // 关闭表单
 const closeDialog = () => {
+  // 重置表单数据
   dialogFormVisible.value = false
-  dept.value = {}
+  deptData.value = {}
+  if(deptForm.value){
+    deptForm.value.resetFields()
+  }
 };
-// 新增部门
-const addEmp = () => {
-  showDialog()
-  setDialogTitle('新增部门')
+// 更新按钮操作
+const setSubmit = (operation) => {
+  submit.value = operation
 }
-// 保存
-const save = async () => {
-  const res = await addDept(dept.value)
 
-  if(res.code){// 成功
-    ElMessage.success("添加成功")
-    closeDialog()
-    await getDeptList()
-  }else {// 失败
+/**
+ * 新增操作
+ * @author BanXia
+ */
+// 新增按钮
+const addEmp = () => {
+
+// 新增部门
+  setDialogTitle('新增部门')
+  setSubmit(save)
+  showDialog()
+}
+// 新增操作
+const save = async () => {
+  // 校验表单项
+  // 如果表单项没有值，则不进行提交
+  if(!deptForm.value)return;
+  deptForm.value.validate(async (validator) => {
+    // validator 是否校验通过
+    if(validator){
+      const res = await addDept(deptData.value)
+      if(res.code){// 成功
+        ElMessage.success("添加成功")
+        closeDialog()
+        await getDeptList()
+      }else {// 失败
+        ElMessage.error(res.msg)
+      }
+    }else{
+      ElMessage.error("表单校验不通过")
+    }
+  })
+
+}
+
+/**
+ * 更新操作
+ */
+// 更新按钮
+const deit = async (deptId) => {
+  // 回显部门信息
+  const res = await queryDeptById(deptId)
+  if(res.code){
+    // 成功
+    deptData.value.name = res.data.name
+    deptData.value.id = res.data.id
+    setDialogTitle('修改部门')
+    setSubmit(update)
+    showDialog()
+  }else{
     ElMessage.error(res.msg)
   }
 }
-// 校验
-const rules = {
-  name: [
-    { required: true, message: "请输入部门名称", trigger: "blur" },
-    { min: 2, max: 10, message: "长度在 2 到 10 个字符之间", trigger: "blur" }
-  ]
-};
+// 更新操作
+const update = async (id) => {
+  // 校验表单项
+  // 如果表单项没有值，则不进行提交
+  if(!deptForm.value)return;
+  deptForm.value.validate(async (validator) => {
+    // validator 是否校验通过
+    if(validator){
+      const res = await updateDept(deptData.value)
+      if(res.code){// 成功
+        ElMessage.success("更新成功")
+        closeDialog()
+        await getDeptList()
+      }else {// 失败
+        ElMessage.error(res.msg)
+      }
+    }else{
+      ElMessage.error("表单校验不通过")
+    }
+  })
+
+}
+
+
 
 </script>
 
@@ -113,12 +188,12 @@ const rules = {
       />
       <el-table-column label="操作" align="center">
         <template #default="scope">
-          <el-button type="primary" size="small"
-            ><el-icon><Edit /></el-icon>编辑</el-button
-          >
-          <el-button type="danger" size="small"
-            ><el-icon><Delete /></el-icon>删除</el-button
-          >
+          <el-button type="primary" size="small" @click = deit(scope.row.id) >
+            <el-icon><Edit /></el-icon>编辑
+          </el-button>
+          <el-button type="danger" size="small">
+            <el-icon><Delete /></el-icon>删除
+          </el-button>
         </template>
       </el-table-column>
     </el-table>
@@ -126,14 +201,14 @@ const rules = {
 
 <!--新增&修改对话框-->
   <el-dialog v-model="dialogFormVisible" :title="dialogTitle" width="500">
-    <el-form :model="dept" :rules="rules">
+    <el-form :model="deptData" :rules="rules" ref = "deptForm">
       <el-form-item label="部门名称" :label-width="80" prop="name">
-        <el-input v-model="dept.name"/>
+        <el-input v-model="deptData.name"/>
       </el-form-item>
     </el-form>
     <template #footer>
         <el-button @click="closeDialog">取消</el-button>
-        <el-button type="primary" @click="save">确定</el-button>
+        <el-button type="primary" @click="submit()">确定</el-button>
     </template>
   </el-dialog>
 </template>
